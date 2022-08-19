21c5f94f02f46b67b359a5d8a8e37597bc21e91b
vincentDubresson <vincent.dubresson@orange.fr>
2022-08-19 15:51:33 +0200

    Add first variables, background and logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World !</h1>
+      <h1>Hello world !</h1>
+      <p>Hello World !</p>
     </div>
   );
 }
