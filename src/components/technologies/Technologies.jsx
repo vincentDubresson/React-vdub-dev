6a3b121e5ff901b1ea6099755cd7e86373346734
vincentDubresson <vincent.dubresson@orange.fr>
2022-08-22 21:34:53 +0200

    Last part : Integration of the icon grid for tablet and desktop + color change on hover

--- src/components/technologies/Technologies.jsx
+++ src/components/technologies/Technologies.jsx
@@ -1,32 +1,28 @@
 /* eslint-disable jsx-a11y/mouse-events-have-key-events */
-import React, { useState } from 'react';
+import React from 'react';
 import './Technologies.scss';
 import { TechLightIcons } from '../../data/Technologies';
 
 export default function Technologies() {
-  const [isHover, setIsHover] = useState(false);
-
-  const mouseOver = () => {
-    setIsHover(true);
-  };
-
-  const mouseOut = () => {
-    setIsHover(false);
-  };
-
   return (
-    <div className="AppTechnologies">
+    <section className="AppTechnologies">
       <h2 className="AppTechnologiesTitle">Techologies</h2>
       <div className="AppTechnologiesContainer">
         {
             TechLightIcons.map((Icon) => (
               <div key={Icon.number} className="AppTechnologiesIcons">
-                <img className="" onMouseOver={mouseOver} onMouseOut={mouseOut} src={(isHover) ? Icon.sourceLight : Icon.sourceDark} alt={Icon.alt} />
+                <img
+                  className="AppTechnologiesIcon"
+                  onMouseEnter={(e) => { e.currentTarget.src = Icon.sourceLight; }}
+                  onMouseLeave={(e) => { e.currentTarget.src = Icon.sourceDark; }}
+                  src={Icon.sourceDark}
+                  alt={Icon.alt}
+                />
                 <p>{Icon.description}</p>
               </div>
             ))
         }
       </div>
-    </div>
+    </section>
   );
 }
