baed3f896899c0913790761fc7c0f4069f23b0fa
vincentDubresson <vincent.dubresson@orange.fr>
2022-08-22 13:59:14 +0200

    second part : Display of the mobile navbar on scroll

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.scss';
 import { NavbarIcons } from '../../data/NavbarIcons';
 
 export default function Navbar() {
+  const [navbarDisplay, setNavbarDisplay] = useState('AppNavbar navbarBottom');
+
+  const navbarOpacity = () => {
+    if (window.scrollY > 300 && window.innerWidth < 600) {
+      setNavbarDisplay('AppNavbar');
+    } else {
+      setNavbarDisplay('AppNavbar navbarBottom');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', navbarOpacity);
+    return () => {
+      window.removeEventListener('scroll', navbarOpacity);
+    };
+  }, []);
+
   return (
-    <nav className="AppNavbar">
+    <nav className={navbarDisplay}>
       {NavbarIcons.map((Icon) => (
         <div key={Icon.number} className="AppNavbarButton">
-          <img className="AppNavBarIcon" src={Icon.source} alt={Icon.alt} />
+          <img
+            className="AppNavbarIcon"
+            src={Icon.source}
+            alt={Icon.alt}
+          />
         </div>
       ))}
     </nav>
