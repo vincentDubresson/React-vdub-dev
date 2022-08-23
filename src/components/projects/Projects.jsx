fbd610a6f18271ebdac0e8cd3a2122ceeda42913
vincentDubresson <vincent.dubresson@orange.fr>
2022-08-23 10:07:50 +0200

    First part : Skeletton of the projects part

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Projects.scss';
+
+export default function Projects() {
+  return (
+    <section id="app_projects" className="AppProjects">
+      <h2 className="AppProjectsTitle">Mes réalisations</h2>
+    </section>
+  );
+}
