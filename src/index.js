f5d554d1e75209aa4a29c09ad4565b9a8f40233c
vincentDubresson <vincent.dubresson@orange.fr>
2022-08-25 16:56:17 +0200

    Add a nasty link for nasty paths

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import wrongWay from './assets/images/background/wrong-way.gif';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route
+        path="*"
+        element={(
+          <main style={{
+            backgroundColor: '#F0F2F3',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100vw',
+            height: '100vh'
+          }}
+          >
+            <img
+              style={{
+                width: '90%',
+                maxWidth: '600px'
+              }}
+              src={wrongWay}
+              alt="Error 404. Not found"
+            />
+          </main>
+        )}
+      />
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
