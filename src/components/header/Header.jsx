7a0d350cb81b245a69e35d8b626000b7a93c3d95
vincentDubresson <vincent.dubresson@orange.fr>
2022-08-25 14:11:41 +0200

    HEADER : Delete useless Link and transfer scroll function in parent div

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-import { Link } from 'react-router-dom';
 import React from 'react';
 import './Header.scss';
 import websiteLogo from '../../assets/images/logo/logo.gif';
@@ -8,6 +7,7 @@ export default function Header(props) {
   const { headerRef } = props;
   const { projectsRef } = props;
   const { contactRef } = props;
+
   const handleScroll = (ref) => {
     window.scrollTo({
       top: ref.offsetTop,
@@ -21,8 +21,8 @@ export default function Header(props) {
       <img className="AppHeaderLogo" src={websiteLogo} alt="Logo du site" />
       <div className="AppHeaderTitleContainer">
         <h1 className="AppHeaderTitle">Bienvenue sur mon Portfolio</h1>
-        <button className="AppHeaderProjectButton" type="button"><Link className="AppHeaderButtonLink" to="/#projects" onClick={() => { handleScroll(projectsRef.current); }}>Mes réalisations</Link></button>
-        <button className="AppHeaderContactButton" type="button"><Link className="AppHeaderButtonLink" to="/#contact" onClick={() => { handleScroll(contactRef.current); }}>Me contacter</Link></button>
+        <button className="AppHeaderProjectButton" type="button" onClick={() => { handleScroll(projectsRef.current); }}>Mes réalisations</button>
+        <button className="AppHeaderContactButton" type="button" onClick={() => { handleScroll(contactRef.current); }}>Me contacter</button>
       </div>
     </div>
   );
